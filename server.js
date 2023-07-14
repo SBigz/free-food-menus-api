a17cde1b62d60b2f579141fd45750f2d44c3dc44
SBigz <Sacha.bigou@gmail.com>
2023-07-14 18:38:52 +0200

    Vercel.json

--- server.js
+++ server.js
@@ -5,6 +5,24 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200;
 
 server.use(middlewares);
+
+server.use((req, res, next) => {
+  const { food } = req.query;
+  
+  // Vérifier si le paramètre 'food' est présent
+  if (food) {
+    // Filtrez les données en fonction de la catégorie spécifiée (par exemple, 'food=desserts')
+    const filteredData = router.db.get("db")
+      .filter(item => item.food === food)
+      .value();
+
+    // Remplacez les données de la réponse par les données filtrées
+    res.locals.data = filteredData;
+  }
+
+  next();
+});
+
 server.use(router);
 
-server.listen(port,()=>console.log(`server is listeneing on port ${port}`));
+server.listen(port, () => console.log(`Server is listening on port ${port}`));
