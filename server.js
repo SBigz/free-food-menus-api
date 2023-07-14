b057aebeface0fc7d9331f75ea82a771b853deca
SBigz <Sacha.bigou@gmail.com>
2023-07-14 19:03:00 +0200

    Vercel.json

--- server.js
+++ server.js
@@ -1,21 +1,10 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router("db.json"); 
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200;
 
 server.use(middlewares);
-
-server.use((req, res, next) => {
-  const food = req.query.food;
-  if (food) {
-    const data = router.db.get(food).value();
-    res.json(data);
-  } else {
-    next();
-  }
-});
-
 server.use(router);
 
-server.listen(port, () => console.log(`Server is listening on port ${port}`));
+server.listen(port,()=>console.log(`server is listeneing on port ${port}`));
